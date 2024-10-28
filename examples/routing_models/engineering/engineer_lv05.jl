f86a35d1239db634abd7d35d02bb104c648198e3
achiang17 <andrewchiang17@gmail.com>
2024-10-28 15:26:23 -0700

    run engineering script, add dependencies

--- examples/routing_models/engineering/engineer_lv05.jl
+++ examples/routing_models/engineering/engineer_lv05.jl
@@ -1,4 +1,4 @@
-using Rivers
+using ClimaRivers
 using Dates
 using TOML
 
--- examples/routing_models/routing/gamma-IRF/route_lv05.jl
+++ examples/routing_models/routing/gamma-IRF/route_lv05.jl
@@ -4,7 +4,6 @@ include("../../../../src/routing/routing.jl")
 
 function main()
     # Unpack config
-    #examples/routing_models/routing/
     config = TOML.parsefile("examples/routing_models/routing/gamma-IRF/Config.toml")
     base = config["base"]["base"]
     hydro_lv = config["level"]["hydro_lv"]
