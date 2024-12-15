3f6fea24da5a0970f1ca76f63dfdc7db59291d99
odunbar <odunbar@caltech.edu>
2024-12-14 16:27:45 -0800

    fix defaults, and args for test pass (t_max added)

--- test/hillslope_channel_model.jl
+++ test/hillslope_channel_model.jl
@@ -17,22 +17,26 @@ end
     FT = Float32
     shape = FT(1.5)
     timescale = FT(1.0)
-    slope1 = MizurouteHillslopeV1(shape, timescale)
+    t_max_hs = FT(60.0)
+    slope1 = MizurouteHillslopeV1(shape, timescale, t_max_hs)
     slope2 = MizurouteHillslopeV1{FT}()
     @test slope1.shape == slope2.shape
     @test slope1.timescale == slope2.timescale
+    @test slope1.t_max == slope2.t_max
     @test slope1 == slope2
+    
 
     # Channel
     FT = Float64
     wave_velocity = FT(1.5 * 86400)
-    diffusivity = FT(8000 * 86400)
-    channel1 = MizurouteChannelV1(wave_velocity, diffusivity)
+    diffusivity = FT(800 * 86400)
+    t_max_ch = FT(120.0)
+    channel1 = MizurouteChannelV1(wave_velocity, diffusivity, t_max_ch)
     channel2 = MizurouteChannelV1{FT}()
     @test channel1.wave_velocity == channel2.wave_velocity
     @test channel1.diffusivity == channel2.diffusivity
+    @test channel1.t_max == channel2.t_max
     @test channel1 == channel2
-
 
 
 end
