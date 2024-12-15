45eb09c8e58aa948cfd626becb8d9b3e5d6b5cc7
odunbar <odunbar@caltech.edu>
2024-12-14 17:35:48 -0800

    format

--- src/Routing.jl
+++ src/Routing.jl
@@ -1,8 +1,6 @@
 # general methods for routing rivers
 export compute_streamflow,
-    compute_river_state,
-    compute_channel_state,
-    compute_hillslope_state
+    compute_river_state, compute_channel_state, compute_hillslope_state
 
 using CSV, DataFrames, Dates, DSP, SpecialFunctions
 
