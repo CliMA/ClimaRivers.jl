4dd7f57af442a4c96603e5a41f22223139a295eb
achiang17 <andrewchiang17@gmail.com>
2025-01-23 00:46:34 -0800

    remove_test, create DateWindow Unit Test

--- test/hillslope_channel_model.jl
+++ test/hillslope_channel_model.jl
@@ -1,15 +1,7 @@
 using Test
-using JLD2
 using ClimaRivers #(gather up the exported functions and structs for use in this scope)
 
-@testset "Hillslope channel model tests" begin
-
-    # @test A == B
-    # @test_throws ErrorType f(x) 
-
-
-end
-
+include("test_gamma_IRF.jl")
 
 @testset "Mizuroute tests" begin
 
@@ -40,7 +32,39 @@ end
 
 end
 
+@testset "DateWindow Tests" begin
+    # Test functions concerning the DateWindow Struct
+    start_date = Date("1996-01-01", "yyy-mm-dd")
+    end_date = Date("1996-01-10", "yyy-mm-dd")
+    date_step = Day(1)
+    dw = DateWindow(
+        start_date = start_date,
+        end_date = end_date,
+        date_step = date_step,
+    )
+    # Initializing DateWindow Test
+    @test start_date == dw.start_date
+    @test end_date == dw.end_date
+    @test date_step == dw.date_step
+
+    # get_all_dates() Test
+    all_dates = get_all_dates(dw)
+    @test all_dates == collect((dw.start_date):(dw.date_step):(dw.end_date))
+
+    # iterate window Test
+    next_dw = iterate(dw)
+    true_next_dw = DateWindow(
+        start_date = Date("1996-01-02", "yyy-mm-dd"),
+        end_date = Date("1996-01-11", "yyy-mm-dd"),
+        date_step = Day(1),
+    )
+    @test next_dw == true_next_dw
+end
+
 @testset "Routing Tests" begin
+    # Test functions inside of Routing.jl
+
+    # Test get_upstream_basins()
     graph_dict =
         Dict("1" => [], "2" => [], "3" => [], "4" => [1, 2], "5" => [3, 4])
     graph_dict = Dict{String, Any}(graph_dict)
@@ -50,17 +74,25 @@ end
     @test sort(upstream1) == []
     @test sort(upstream4) == [1, 2]
     @test sort(upstream5) == [1, 2, 3, 4]
+
+    
+    # Test compute_river_state()
+    initial_window, river_model, env, data_end_date, _ = test_run_init()
+    
+    ## compute_hillslope_state()
+    hillslope_model = river_model.hillslope_model
+    @test compute_hillslope_state(initial_window, hillslope_model, env) == compute_hillslope_state(initial_window, hillslope_model, env.static_env, env.dynamic_env)
+    # new_state = compute_hillslope_state(initial_window, river_model, env)
+    
+
+
+    ## computer_channel_state()
+
 end
 
 @testset "Small Example Tests" begin
-    # test running of mini test to make sure output is correct
-    # can use small chunk of time 2-10 days or can adjust t_max to be like 25
-    data_file_path = joinpath(@__DIR__, "..", "mini_data", "routing")
-    @info "reading data files from $(data_file_path)"
-    simulation_file = joinpath(data_file_path, "simulations", "simulations_lv05", "gamma_IRF", "streamflow_history6 days.jld2")
-    file = jldopen(simulation_file, "r")  
-    streamflow = file["streamflows"] 
-    close(file) 
+    # Run entire small sample set example (10 day span, window size 6)
+    streamflows, river_states = test_run_full()
     expected_streamflows = Dict(
         1.0514351e9 => 0.1681771926595176,
         1.05146043e9 => 9.439266521834611,
@@ -68,5 +100,5 @@ end
         1.05143511e9 => 4.791551273615758,
         1.05142965e9 => 133.05341709808118
     )
-    @test streamflow[end] == expected_streamflows
+    @test streamflows[end] == expected_streamflows
 end
