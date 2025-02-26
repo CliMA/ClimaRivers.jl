5d8a74255a730d122ca4e0f00fac325eae18241a
achiang17 <andrewchiang17@gmail.com>
2025-02-26 14:41:23 -0800

    join split json files into one mapping and update visualization

---
+++ examples/mapping/join_json_files.jl
@@ -0,0 +1,32 @@
+using JSON
+
+function merge_json_files(output_file::String, input_files::Vector{String})
+    merged_data = Dict()
+
+    for file in input_files
+        data = JSON.parsefile(file)
+        merge!(merged_data, data)
+    end
+
+    open(output_file, "w") do io
+        JSON.print(io, merged_data)
+    end
+end
+
+# Example usage:
+data_file_path = joinpath(@__DIR__, "..", "..", "data")
+input_files = [
+    joinpath(
+        data_file_path,
+        "midway_data",
+        "mapping_dicts",
+        "dict$(lpad(i, 2, '0')).json",
+    ) for i in 1:32
+]
+output_file = joinpath(
+    data_file_path,
+    "midway_data",
+    "mapping_dicts",
+    "era5_grid_to_basin_dict.json",
+)
+merge_json_files(output_file, input_files)
